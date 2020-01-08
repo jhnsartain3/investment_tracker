ad3f1b3e9177c3cd111d477d90cb98cb39735d9b
jhnsartain3 <=>
2020-01-07 20:44:37 -0600

    Updates

---
+++ precache-manifest.f3d8c37ccd1ca8c2a65435d718b7a28b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "161c739934cfea78c4849bc2f62f518f",
+    "url": "/investment_tracker/static/media/shards-dashboards-logo.161c7399.svg"
+  },
+  {
+    "revision": "73476783ca0f7583198978de7073815b",
+    "url": "/investment_tracker/static/media/0.73476783.jpg"
+  },
+  {
+    "revision": "cb84bc004bcb0f663bed",
+    "url": "/investment_tracker/static/js/runtime~main.cb84bc00.js"
+  },
+  {
+    "revision": "18accc7169c094124843",
+    "url": "/investment_tracker/static/js/main.18accc71.chunk.js"
+  },
+  {
+    "revision": "c99dd85dc2f9e1424e37",
+    "url": "/investment_tracker/static/js/1.c99dd85d.chunk.js"
+  },
+  {
+    "revision": "18accc7169c094124843",
+    "url": "/investment_tracker/static/css/main.d7e464b2.chunk.css"
+  },
+  {
+    "revision": "c99dd85dc2f9e1424e37",
+    "url": "/investment_tracker/static/css/1.97e2d3a2.chunk.css"
+  },
+  {
+    "revision": "ba420a44b111ec42066ddfeecc7accc6",
+    "url": "/investment_tracker/index.html"
+  }
+];
