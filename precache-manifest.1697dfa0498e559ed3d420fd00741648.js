af46f0aac6db2fbcb2fa53f7ce056d76143b0571
jhnsartain3 <=>
2020-01-08 18:01:26 -0600

    Updates

--- precache-manifest.1697dfa0498e559ed3d420fd00741648.js
+++ precache-manifest.1697dfa0498e559ed3d420fd00741648.js
@@ -12,15 +12,15 @@ self.__precacheManifest = [
     "url": "/investment_tracker/static/js/runtime~main.cb84bc00.js"
   },
   {
-    "revision": "a1fe9eff87f47d4a17db",
-    "url": "/investment_tracker/static/js/main.a1fe9eff.chunk.js"
+    "revision": "3b50db3531b2cdaa53fb",
+    "url": "/investment_tracker/static/js/main.3b50db35.chunk.js"
   },
   {
     "revision": "c99dd85dc2f9e1424e37",
     "url": "/investment_tracker/static/js/1.c99dd85d.chunk.js"
   },
   {
-    "revision": "a1fe9eff87f47d4a17db",
+    "revision": "3b50db3531b2cdaa53fb",
     "url": "/investment_tracker/static/css/main.d7e464b2.chunk.css"
   },
   {
@@ -28,7 +28,7 @@ self.__precacheManifest = [
     "url": "/investment_tracker/static/css/1.97e2d3a2.chunk.css"
   },
   {
-    "revision": "2ea513593c6763a7f837db8a4a8205ce",
+    "revision": "2336610041b7287526777288c753e8ba",
     "url": "/investment_tracker/index.html"
   }
 ];
