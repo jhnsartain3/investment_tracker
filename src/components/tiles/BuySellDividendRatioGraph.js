144193015bbf738751b192b2020d3fc13aaae028
jhnsartain3 <=>
2020-01-19 00:05:29 -0600

    graph ratio of buy/sell/dividend

---
+++ src/components/tiles/BuySellDividendRatioGraph.js
@@ -0,0 +1,97 @@
+import React from "react";
+import PropTypes from "prop-types";
+import {Card, CardBody, CardFooter, CardHeader, Col, FormSelect, Row} from "shards-react";
+
+import Chart from "../utils/chart";
+
+class BuySellDividendRatioGraph extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.canvasRef = React.createRef();
+    }
+
+    componentDidMount() {
+        const chartConfig = {
+            type: "pie",
+            data: this.props.chartData,
+            options: {
+                ...{
+                    legend: {
+                        position: "bottom",
+                        labels: {
+                            padding: 25,
+                            boxWidth: 20
+                        }
+                    },
+                    cutoutPercentage: 0,
+                    tooltips: {
+                        custom: false,
+                        mode: "index",
+                        position: "nearest"
+                    }
+                },
+                ...this.props.chartOptions
+            }
+        };
+
+        new Chart(this.canvasRef.current, chartConfig);
+    }
+
+    render() {
+        const {title} = this.props;
+        return (
+            <Card small className="h-100">
+                <CardHeader className="border-bottom">
+                    <h6 className="m-0">{title}</h6>
+                </CardHeader>
+                <CardBody className="d-flex py-0">
+                    <canvas
+                        height="220"
+                        ref={this.canvasRef}
+                        className="blog-users-by-device m-auto"
+                    />
+                </CardBody>
+            </Card>
+        );
+    }
+}
+
+BuySellDividendRatioGraph.propTypes = {
+    /**
+     * The component's title.
+     */
+    title: PropTypes.string,
+    /**
+     * The chart config object.
+     */
+    chartConfig: PropTypes.object,
+    /**
+     * The Chart.js options.
+     */
+    chartOptions: PropTypes.object,
+    /**
+     * The chart data.
+     */
+    chartData: PropTypes.object
+};
+
+BuySellDividendRatioGraph.defaultProps = {
+    title: "Buy/Sell/Dividend Ratio",
+    chartData: {
+        datasets: [
+            {
+                hoverBorderColor: "#ffffff",
+                data: [50, 40, 10],
+                backgroundColor: [
+                    "rgba(0,123,255,0.6)",
+                    "rgba(0,123,255,0.9)",
+                    "rgba(0,123,255,0.2)"
+                ]
+            }
+        ],
+        labels: ["Buy", "Sell", "Dividend"]
+    }
+};
+
+export default BuySellDividendRatioGraph;
--- src/pages/AllTransactionsScreen.js
+++ src/pages/AllTransactionsScreen.js
@@ -1,23 +1,95 @@
 import React from "react";
-import {Col, Container, Row} from "shards-react";
+import {Card, CardHeader, Col, Container, Row} from "shards-react";
 
 import PageTitle from "../components/common/PageTitle";
 import AllTransactionsTable from "../components/tiles/AllTransactionsTable";
+import BuySellDividendRatioGraph from "../components/tiles/BuySellDividendRatioGraph";
+import AccessApiWrapper from "../components/api/AccessApiWrapper";
 
-const AllTransactionsScreen = () => (
-    <Container fluid className="main-content-container px-4">
-        {/* Page Header */}
-        <Row noGutters className="page-header py-4">
-            <PageTitle sm="4" title="View All Transactions" subtitle="Transactions" className="text-sm-left"/>
-        </Row>
+var accessApiWrapper = new AccessApiWrapper();
 
-        {/* List */}
-        <Row>
-            <Col lg="8" md="12" sm="12" className="mb-4">
-                <AllTransactionsTable/>
-            </Col>
-        </Row>
-    </Container>
-);
+class AllTransactionsScreen extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            chartData: null,
+            isLoaded: false
+        }
+    }
+
+    componentWillMount() {
+        accessApiWrapper.getData("/Transaction").then((result) => {
+            let buyCount = 0;
+            let sellCount = 0;
+            let dividendCount = 0;
+
+            result.forEach((x) => {
+                if ((x.type) === "buy") buyCount++;
+                if ((x.type) === "sell") sellCount++;
+                if ((x.type) === "dividend") dividendCount++;
+            });
+
+            var chartData = {
+                datasets: [
+                    {
+                        hoverBorderColor: "#ffffff",
+                        data: [buyCount, sellCount, dividendCount],
+                        backgroundColor: [
+                            "rgba(0,123,255,0.6)",
+                            "rgba(0,123,255,0.9)",
+                            "rgba(0,123,255,0.2)"
+                        ]
+                    }
+                ],
+                labels: ["Buy", "Sell", "Dividend"]
+            };
+
+            this.setState({chartData, isLoaded: true});
+        });
+    }
+
+    render() {
+        const {isLoaded} = this.state;
+
+        if (isLoaded)
+            return (
+                <Container fluid className="main-content-container px-4">
+                    {/* Page Header */}
+                    <Row noGutters className="page-header py-4">
+                        <PageTitle sm="4" title="View All Transactions" subtitle="Transactions"
+                                   className="text-sm-left"/>
+                    </Row>
+
+                    {/* List */}
+                    <Row>
+                        <Col lg="8" md="12" sm="12" className="mb-4">
+                            <AllTransactionsTable/>
+                        </Col>
+
+                        {/* Ratio buy/sell */}
+                        <Col lg="4" md="6" sm="12" className="mb-4">
+                            <BuySellDividendRatioGraph chartData={this.state.chartData}/>
+                        </Col>
+                    </Row>
+                </Container>
+            );
+        else return (
+            <Container fluid className="main-content-container px-4">
+                <Row noGutters className="page-header py-4">
+                    <PageTitle sm="4" title="View All Transactions" subtitle="Transactions"
+                               className="text-sm-left"/>
+                </Row>
+                <Row>
+                    <Card small className="mb-4">
+                        <CardHeader>
+                            <h1 className="m-0">Loading...</h1>
+                        </CardHeader>
+                    </Card>
+                </Row>
+            </Container>
+        );
+    }
+}
 
 export default AllTransactionsScreen;
