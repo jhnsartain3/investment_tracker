0b36221c2226a1c43e9bd635fc3583b2a0b7b1a5
John Sartain <jhnsartain3@gmail.com>
2020-02-23 21:13:54 -0600

    set user profile to a generic image

--- src/components/layout/MainNavbar/NavbarNav/UserActions.js
+++ src/components/layout/MainNavbar/NavbarNav/UserActions.js
@@ -28,7 +28,7 @@ export default class UserActions extends React.Component {
                 <DropdownToggle caret tag={NavLink} className="text-nowrap px-3">
                     <img
                         className="user-avatar rounded-circle mr-2"
-                        src={require("../../../../assets/images/avatars/0.jpg")}
+                        src={require("../../../../assets/images/generic-user-icon.jpg")}
                         alt="User Avatar"
                     />{" "}
                     <span className="d-none d-md-inline-block">{AuthService.getProfile().given_name[0]}</span>
