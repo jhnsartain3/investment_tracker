30d2b2421808b496c6e8dea493405e11bb05e2ca
John Sartain <jhnsartain3@gmail.com>
2020-02-23 21:14:37 -0600

    add ability to select a stock to view on StockOverviewScreen

---
+++ src/components/tiles/StockPickerDropdown.js
@@ -0,0 +1,74 @@
+import {Dropdown, DropdownItem, DropdownMenu, DropdownToggle, FormInput, InputGroup, Row} from "shards-react";
+import React from "react";
+
+export default class StockPickerDropdown extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            placeholder: "Some Ticker...",
+            open: false
+        };
+
+        this.toggle = this.toggle.bind(this);
+        this.populateList = this.populateList.bind(this);
+        this.updateSelectedItem = this.updateSelectedItem.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    toggle() {
+        this.setState(prevState => {
+            return {open: !prevState.open};
+        });
+    }
+
+    handleChange(event) {
+        this.setState(
+            {
+                selectedItem: event.target.value
+            }
+        );
+
+        this.props.onTickerSelected(event.target.value)
+    }
+
+    updateSelectedItem(event) {
+        event.preventDefault();
+
+        this.setState({
+            selectedItem: event.target.name,
+        });
+
+        this.props.onTickerSelected(event.target.name)
+    }
+
+    populateList() {
+        var tickerList = this.props.tickerList;
+
+        if (tickerList === null || tickerList.length === 0) {
+            return (
+                <DropdownItem>Tickers will appear here when you have transactions added to your account</DropdownItem>)
+        }
+        return this.props.tickerList.map((x) =>
+            <DropdownItem name={x} onClick={this.updateSelectedItem}>{x}</DropdownItem>
+        )
+    }
+
+    render() {
+        return (
+            <div>
+                <InputGroup className="mb-3">
+                    <FormInput onChange={this.handleChange}
+                               placeholder={this.state.placeholder} value={this.state.selectedItem}/>
+                    <Dropdown
+                        open={this.state.open} toggle={this.toggle}>
+                        <DropdownToggle caret>Tickers</DropdownToggle>
+                        <DropdownMenu small>
+                            {this.populateList()}
+                        </DropdownMenu>
+                    </Dropdown>
+                </InputGroup>
+            </div>
+        );
+    }
+}
--- src/pages/StockOverviewScreen.js
+++ src/pages/StockOverviewScreen.js
@@ -1,135 +1,58 @@
-import React from "react";
-import PropTypes from "prop-types";
+import React, {Component} from "react";
 import {Col, Container, Row} from "shards-react";
 
 import PageTitle from "./../components/common/PageTitle";
 import StockGraph from "../components/tiles/StockGraph";
 import SubmitNewTransaction from "../components/tiles/SubmitNewTransaction";
+import StockPickerDropdown from "../components/tiles/StockPickerDropdown";
+import AccessApiWrapper from "../components/api/AccessApiWrapper";
 
-const StockOverviewScreen = ({smallStats}) => (
-    <Container fluid className="main-content-container px-4">
-        {/* Page Header */}
-        <Row noGutters className="page-header py-4">
-            <PageTitle title="Some Stock Name" subtitle="" className="text-sm-left mb-3"/>
-        </Row>
+const accessApiWrapper = new AccessApiWrapper();
 
-        <Row>
-            {/* Editor */}
-            <Col lg="9" md="12" sm="12" className="mb-4">
-                <StockGraph/>
-            </Col>
+class StockOverviewScreen extends Component {
+    constructor(props) {
+        super(props);
 
-            {/* Groups */}
-            <Col lg="3" md="6" sm="12" className="mb-4">
-                <SubmitNewTransaction ticker="test"/>
-            </Col>
-        </Row>
-    </Container>
-);
-
-StockOverviewScreen.propTypes = {
-    /**
-     * The small stats dataset.
-     */
-    smallStats: PropTypes.array
-};
-
-StockOverviewScreen.defaultProps = {
-    smallStats: [
-        {
-            label: "Posts",
-            value: "2,390",
-            percentage: "4.7%",
-            increase: true,
-            chartLabels: [null, null, null, null, null, null, null],
-            attrs: {md: "6", sm: "6"},
-            datasets: [
-                {
-                    label: "Today",
-                    fill: "start",
-                    borderWidth: 1.5,
-                    backgroundColor: "rgba(0, 184, 216, 0.1)",
-                    borderColor: "rgb(0, 184, 216)",
-                    data: [1, 2, 1, 3, 5, 4, 7]
-                }
-            ]
-        },
-        {
-            label: "Pages",
-            value: "182",
-            percentage: "12.4",
-            increase: true,
-            chartLabels: [null, null, null, null, null, null, null],
-            attrs: {md: "6", sm: "6"},
-            datasets: [
-                {
-                    label: "Today",
-                    fill: "start",
-                    borderWidth: 1.5,
-                    backgroundColor: "rgba(23,198,113,0.1)",
-                    borderColor: "rgb(23,198,113)",
-                    data: [1, 2, 3, 3, 3, 4, 4]
-                }
-            ]
-        },
-        {
-            label: "Comments",
-            value: "8,147",
-            percentage: "3.8%",
-            increase: false,
-            decrease: true,
-            chartLabels: [null, null, null, null, null, null, null],
-            attrs: {md: "4", sm: "6"},
-            datasets: [
-                {
-                    label: "Today",
-                    fill: "start",
-                    borderWidth: 1.5,
-                    backgroundColor: "rgba(255,180,0,0.1)",
-                    borderColor: "rgb(255,180,0)",
-                    data: [2, 3, 3, 3, 4, 3, 3]
-                }
-            ]
-        },
-        {
-            label: "New Customers",
-            value: "29",
-            percentage: "2.71%",
-            increase: false,
-            decrease: true,
-            chartLabels: [null, null, null, null, null, null, null],
-            attrs: {md: "4", sm: "6"},
-            datasets: [
-                {
-                    label: "Today",
-                    fill: "start",
-                    borderWidth: 1.5,
-                    backgroundColor: "rgba(255,65,105,0.1)",
-                    borderColor: "rgb(255,65,105)",
-                    data: [1, 7, 1, 3, 1, 4, 8]
-                }
-            ]
-        },
-        {
-            label: "Subscribers",
-            value: "17,281",
-            percentage: "2.4%",
-            increase: false,
-            decrease: true,
-            chartLabels: [null, null, null, null, null, null, null],
-            attrs: {md: "4", sm: "6"},
-            datasets: [
-                {
-                    label: "Today",
-                    fill: "start",
-                    borderWidth: 1.5,
-                    backgroundColor: "rgb(0,123,255,0.1)",
-                    borderColor: "rgb(0,123,255)",
-                    data: [3, 2, 3, 2, 4, 5, 4]
-                }
-            ]
+        this.state = {
+            tickerList: [],
+            selectedTicker: "Stock Name"
         }
-    ]
-};
+    }
+
+    receiveStockTicker = (selectedTicker) => {
+        this.setState({selectedTicker: selectedTicker});
+    };
+
+    componentDidMount() {
+        accessApiWrapper.getData("/All-Time-Owned-Companies").then((result) => {
+            this.setState({tickerList: result});
+        });
+    }
+
+    render() {
+        return (
+            <Container fluid className="main-content-container px-4">
+                {/* Page Header */}
+                <Row noGutters className="page-header py-4">
+                    <PageTitle title={this.state.selectedTicker.toUpperCase()} subtitle=""
+                               className="text-sm-left mb-3"/>
+                    <StockPickerDropdown onTickerSelected={this.receiveStockTicker} tickerList={this.state.tickerList}/>
+                </Row>
+
+                <Row>
+                    {/* Editor */}
+                    <Col lg="9" md="12" sm="12" className="mb-4">
+                        <StockGraph/>
+                    </Col>
+
+                    {/* Groups */}
+                    <Col lg="3" md="6" sm="12" className="mb-4">
+                        <SubmitNewTransaction ticker="test"/>
+                    </Col>
+                </Row>
+            </Container>
+        );
+    }
+}
 
 export default StockOverviewScreen;
