bbb5b589d3f504c21a242e4b1cfc6e8a27566a8d
John Sartain <jhnsartain3@gmail.com>
2020-02-28 20:30:57 -0600

    Updates

---
+++ precache-manifest.22df724100bc2750b6d324eb0ce4f7a8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd82ff8867c0f1615d35c202bd73a31b",
+    "url": "/investment_tracker/index.html"
+  },
+  {
+    "revision": "6a84574b1c5fb1239fc3",
+    "url": "/investment_tracker/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "3d3de0fd4e4016dda48b",
+    "url": "/investment_tracker/static/css/main.39157d1d.chunk.css"
+  },
+  {
+    "revision": "6a84574b1c5fb1239fc3",
+    "url": "/investment_tracker/static/js/2.dc3a6a0c.chunk.js"
+  },
+  {
+    "revision": "62e6ba4af1b6154aa64800ad4b837aa3",
+    "url": "/investment_tracker/static/js/2.dc3a6a0c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d3de0fd4e4016dda48b",
+    "url": "/investment_tracker/static/js/main.60d85acf.chunk.js"
+  },
+  {
+    "revision": "9a5da8e2dfafa4c6c865",
+    "url": "/investment_tracker/static/js/runtime-main.e5f8d50a.js"
+  },
+  {
+    "revision": "161c739934cfea78c4849bc2f62f518f",
+    "url": "/investment_tracker/static/media/shards-dashboards-logo.161c7399.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/investment_tracker/precache-manifest.6705b4eadc1acf063676bfe07cdd15fa.js"
+  "/investment_tracker/precache-manifest.22df724100bc2750b6d324eb0ce4f7a8.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/investment_tracker/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/investment_tracker/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
